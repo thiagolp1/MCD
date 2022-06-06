401833f22e6028039abd2fb8d169a881da8309ed
Thiago <Thiagolopespereiralp1@gmail.com>
2022-06-06 19:15:16 -0300

    termino da atividade

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int Dividendo, Divisor, Resto;
+
+Console.WriteLine("Máximo Divisor Comum (método iterativo)\n");
+
+Console.Write("Digite o Dividendo:");
+Dividendo = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o divisor:");
+Divisor = Convert.ToInt32(Console.ReadLine());
+
+do
+{   
+    Resto = Dividendo % Divisor;
+    Dividendo = Divisor;
+    Divisor = Resto;
+}
+while(Resto !=0);
+
+Console.WriteLine($"MDC(a, b) = {Dividendo}");
